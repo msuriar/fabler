7238c5e0f4865027ee6dcad0e4378ebdacdf0917
Murali Suriar <code@suriar.com>
2013-04-15 22:52:26 +0100

    More forking.

--- fabler.c
+++ fabler.c
@@ -5,6 +5,8 @@
 
 void print_args(int argc, char *argv[]);
 void create_child(void);
+int child(void);
+int parent(void);
 
 int main(int argc, char *argv[])
 {
@@ -27,7 +29,25 @@ void create_child(void) {
 
   if (childPid != 0) {
     printf("I'm the parent! My child is %d\n", childPid);
+    parent();
   } else {
-    printf("I'm the child. I have no idea what's going on.");
+    printf("I'm the child. I have no idea what's going on.\n");
+    child();
   }
+}
+
+int child(void) {
+  char *program = "/usr/bin/true";
+  char *nothing[0];
+  int ret;
+  ret = execv(program, nothing);
+  return ret;
+}
+
+int parent(void) {
+  pid_t finished;
+  int status;
+  finished = wait(status);
+  printf("Child process with PID %d terminated with exit status %d\n.", finished, status);
+  return status;
 }

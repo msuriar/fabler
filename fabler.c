37616d4d30c329e4b219bd1b2da572c6dd48d661
Murali Suriar <code@suriar.com>
2013-04-21 22:09:47 +0100

    Lots of cleanup.

--- fabler.c
+++ fabler.c
@@ -17,6 +17,8 @@
 
 #define RIP_METRIC_UNREACH 16
 
+#define SLEEP_INTERVAL 30
+
 struct RIPPacket {
   uint8_t command;
   uint8_t version;
@@ -29,14 +31,34 @@ struct RIPPacket {
   uint32_t metric;
 };
 
-char *get_net_from_prefix(char *prefix);
-int get_prefix_len_from_prefix(char *prefix);
+
+void diep(char *s) {
+  perror(s);
+}
+
+
+char *get_net_from_prefix(char *prefix) {
+  char *loc = strchr(prefix, '/');
+
+  char *network = malloc((loc-prefix)*sizeof(char));
+  strlcpy(network, prefix, loc-prefix+1);
+  return network;
+}
+
+
+int get_prefix_len_from_prefix(char *prefix) {
+  int a,b,c,d,len;
+  sscanf(prefix, "%i.%i.%i.%i/%i", &a, &b, &c, &d, &len);
+  return len;
+}
+
 
 uint32_t prefix_len_to_subnet_mask(int prefix_len) {
     uint32_t netmask = UINT32_MAX;
     netmask <<= 32 - prefix_len;
     return netmask;
 }
+
 
 struct RIPPacket *create_packet(char *prefix, int metric) {
   struct RIPPacket *data = malloc(sizeof(struct RIPPacket));
@@ -64,124 +86,11 @@ struct RIPPacket *create_packet(char *prefix, int metric) {
   return data;
 }
 
+
 void htonRIPPacket(struct RIPPacket *r, char b[BUFLEN]) {
   memcpy(b, r, sizeof(*r));
 }
 
-void print_args(int argc, char *argv[]);
-void create_child(void);
-int child(void);
-int parent(void);
-void send_packet(struct RIPPacket *r);
-
-int run_child(int argc, char *argv[]);
-
-int get_prefix_len_from_prefix(char *prefix) {
-  int a,b,c,d,len;
-  sscanf(prefix, "%i.%i.%i.%i/%i", &a, &b, &c, &d, &len);
-  return len;
-}
-
-char *get_net_from_prefix(char *prefix) {
-  char *loc = strchr(prefix, '/');
-
-  char *network = malloc((loc-prefix)*sizeof(char));
-  strlcpy(network, prefix, loc-prefix+1);
-  return network;
-}
-
-void diep(char *s) {
-  perror(s);
-}
-
-void run_loop(int argc, char *argv[]) {
-  int successes = 0;
-  int failures = 0;
-  int healthy = 0;
-
-  struct RIPPacket *healthy_update = create_packet(argv[1], 1);
-  struct RIPPacket *unhealthy_update = create_packet(argv[1], RIP_METRIC_UNREACH);
-
-  for (int i=0; i < 10; i++) {
-    if (run_child(argc, argv)) {
-      /* Returned non-zero, therefore failure */
-      successes = 0;
-      failures += 1;
-    } else {
-      /* Returned 0; therefore success. */
-      failures = 0;
-      successes += 1;
-    }
-
-    if (successes >= 3) {
-      healthy = 100;
-    }
-
-    if (failures >= 3) {
-      healthy = 0;
-    }
-
-    if (healthy) {
-      send_packet(healthy_update);
-    } else {
-      send_packet(unhealthy_update);
-    }
-    sleep(1);
-  }
-}
-
-void print_args(int argc, char *argv[]) {
-  printf("Hello Murali.\n");
-  printf("The number of arguments is: %d\n", argc);
-  for ( int i = 0; i < argc; i++ ) {
-    printf("argv[%d]: %s\n", i, argv[i]);
-  }
-}
-
-void create_child(void) {
-  pid_t childPid;
-  childPid = vfork();
-
-  if (childPid != 0) {
-    printf("I'm the parent! My child is %d\n", childPid);
-    parent();
-  } else {
-    printf("I'm the child. I have no idea what's going on.\n");
-    child();
-  }
-}
-
-int child(void) {
-  char *program = "true";
-  char *argv[] = { program, NULL };
-  int ret;
-  ret = execvp(program, argv);
-  return ret;
-}
-
-int parent(void) {
-  pid_t finished;
-  int status = 0;
-  finished = wait(&status);
-  printf("Child process with PID %d terminated with exit status %d.\n", finished, status);
-  return status;
-}
-
-int run_child(int argc, char *argv[]) {
-  int status = 0;
-  pid_t childPid;
-
-  childPid = vfork();
-
-  if (childPid != 0) {
-    pid_t finished;
-    finished = wait(&status);
-    return status;
-  } else {
-    execvp(argv[2], &argv[2]);
-    return status;
-  }
-}
 
 void send_packet(struct RIPPacket *r) {
   struct sockaddr_in si_other;
@@ -212,16 +121,69 @@ void send_packet(struct RIPPacket *r) {
   }
 }
 
+
+int run_child(char *argv[]) {
+  int status = 0;
+  pid_t childPid;
+
+  childPid = vfork();
+
+  if (childPid != 0) {
+    pid_t finished;
+    finished = wait(&status);
+    return status;
+  } else {
+    execvp(argv[2], &argv[2]);
+    return status;
+  }
+}
+
+
+void run_loop(char *argv[]) {
+  int successes = 0;
+  int failures = 0;
+  int healthy = 0;
+
+  char *prefix = argv[1];
+
+  struct RIPPacket *healthy_update = create_packet(prefix, 1);
+  struct RIPPacket *unhealthy_update = create_packet(prefix, RIP_METRIC_UNREACH);
+
+  while (1) {
+    if (run_child(argv)) {
+      /* Returned non-zero, therefore failure */
+      successes = 0;
+      failures += 1;
+    } else {
+      /* Returned 0; therefore success. */
+      failures = 0;
+      successes += 1;
+    }
+
+    if (successes >= 3) {
+      healthy = 100;
+    }
+
+    if (failures >= 3) {
+      healthy = 0;
+    }
+
+    if (healthy) {
+      send_packet(healthy_update);
+    } else {
+      send_packet(unhealthy_update);
+    }
+    sleep(SLEEP_INTERVAL);
+  }
+}
+
+
 int main(int argc, char *argv[])
 {
-  //  char *prefix = argv[1];
-  //  printf("prefix: %s\n", prefix);
-  //  char *net = get_net_from_prefix(prefix);
-  //  int pfl = get_prefix_len_from_prefix(prefix);
-  //  printf("net: %s\n", net);
-  //  printf("pfl: %i\n", pfl);
-  char *prefix = argv[1];
-  struct RIPPacket *r = create_packet(prefix, 1);
-  send_packet(r);
+  if (argc < 3) {
+    diep("Insufficient number of arguments!");
+  }
+
+  run_loop(argv);
   exit(0);
 }
